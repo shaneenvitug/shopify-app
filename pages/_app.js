bf1e4f790886367a63b02e3500c2640bcc07e7d8
Shaneen Vitug <shaneenvitug@gmail.com>
2020-05-17 23:25:18 +1000

    set up graphql and apollo and resource list

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,14 @@ import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
 import Cookies from 'js-cookie';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: 'include'
+  },
+});
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
@@ -18,7 +25,9 @@ class MyApp extends App {
         </Head>
         <Provider config={config}>
           <AppProvider i18n={translations}>
+          <ApolloProvider client={client}>
             <Component {...pageProps} />
+          </ApolloProvider>
           </AppProvider>
         </Provider>
       </>
